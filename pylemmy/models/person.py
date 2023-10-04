d3d4bb821f3ddf5ba66c2b01134a64f608558b7d
noenfugler <github@greenwolf.com.au>
2023-10-04 11:40:17 +1100

    Bugfix Fixed bug in models/person.py lint tidy up

--- pylemmy/models/person.py
+++ pylemmy/models/person.py
@@ -7,7 +7,12 @@ from pylemmy import api
 class Person:
     """A class for Persons."""
 
-    def __init__(self, lemmy: "pylemmy.Lemmy", counts: api.person.PersonAggregates, person: api.person.Person):
+    def __init__(
+        self,
+        lemmy: "pylemmy.Lemmy",
+        counts: api.person.PersonAggregates,
+        person: api.person.Person,
+    ):
         """Initialize a Person instance.
 
         :param lemmy: A Lemmy instance.
